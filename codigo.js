ee44c2eb7477c69552608a7dab4286bca381e7e3
Ynara lira ventura <79769248+ynaralira@users.noreply.github.com>
2021-08-19 08:27:29 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,7 @@
+
+alert("SEJA BEM VINDO!!");
+var nome = prompt ("Qual seu nome?");
+alert("Olá " + nome + " conheça nossos produtos!" );
+
+var valortotal = valor*quantidade;
+window.document.getSelection(valor)
